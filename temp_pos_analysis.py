3c3a6b37cde92c3fb44bde63f91cd888ffa97bba
YonatanGideoni <yonatan.gideoni@gmail.com>
2020-05-13 20:57:17 +0300

    Fixed the temperature data

--- temp_pos_analysis.py
+++ temp_pos_analysis.py
@@ -8,8 +8,8 @@ relevant_cols = 'A:D'
 header_row = 1
 
 experiment_data = pd.read_csv(file_path, header=header_row - 1,
-                              usecols=['Time (s)', 'Position (m)', 'Temperature (°C)']) \
-    .rename(columns={'Time (s)': 'time', 'Position (m)': 'pos', 'Temperature (°C)': 'temp'})
+                              usecols=['Time (s)', 'Position (m)', 'Temperature (C)']) \
+    .rename(columns={'Time (s)': 'time', 'Position (m)': 'pos', 'Temperature (C)': 'temp'})
 
 # smoothen the position data and show the unsmoothed and smoothed data on the same graph
 axes = plt.gca()
