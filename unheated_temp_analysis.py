812e203e970a55d5bb8cd2e0ddb7ce193fd3898c
YonatanGideoni <yonatan.gideoni@gmail.com>
2020-05-15 00:09:14 +0300

    Added start time

--- unheated_temp_analysis.py
+++ unheated_temp_analysis.py
@@ -12,8 +12,11 @@ header_row = 1
 experiment_data = pd.read_csv(file_path, header=header_row - 1, usecols=['Time (s)', 'Temperature (C)']) \
     .rename(columns={'Time (s)': 'time', 'Temperature (C)': 'temp'})
 
-end_temp = 22.9  # [C]
-experiment_data = experiment_data[experiment_data.temp > end_temp]  # assuming that temperature doesn't rise later...
+end_temp = 25  # [C]
+start_time = 0  # [s]
+# assuming that temperature doesn't rise later...
+experiment_data = experiment_data[(experiment_data.temp > end_temp) & (experiment_data.time > start_time)]
+experiment_data.time -= start_time
 
 experiment_data.plot(x='time', y='temp', marker='.', linestyle='None', label='Experimental Data')
 
@@ -21,7 +24,7 @@ experiment_data.plot(x='time', y='temp', marker='.', linestyle='None', label='Ex
 data_func_to_fit = lambda t, amp, decay_time: amp * np.exp(-t / decay_time) + end_temp
 [amplitude, decay_time], errors = fit_func(data_func_to_fit, experiment_data.time, experiment_data.temp)
 
-time_range = [0, experiment_data.time.max()]
+time_range = [experiment_data.time.min(), experiment_data.time.max()]
 fitted_data_label = 'Function fitted to data \n' \
     f'({amplitude:.2f}$\pm${errors[0]:.2f})exp(-t/({decay_time:.0f}$\pm${errors[1]:.0f}))+{end_temp}'
 plot_func(lambda t: data_func_to_fit(t, amplitude, decay_time), time_range, c='k', linestyle='dashed',
