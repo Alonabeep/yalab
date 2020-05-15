0f76791efa478bf5d13c01693b7265999bec44a9
Alonabeep <alon.avni@walla.co.il>
2020-05-15 17:33:00 +0300

    nothing new

--- temp_pos_analysis.py
+++ temp_pos_analysis.py
@@ -1,8 +1,7 @@
 import pandas as pd
+import python3_utils as utils
 
-from utils import *
-
-file_path = '..\\Results\\13.5.2020\\ex1 grouped - 13.5.2020.csv'
+file_path = 'ex1_grouped_13_5_2020.csv'
 
 relevant_cols = 'A:D'
 header_row = 1
@@ -12,10 +11,10 @@ experiment_data = pd.read_csv(file_path, header=header_row - 1,
     .rename(columns={'Time (s)': 'time', 'Position (m)': 'pos', 'Temperature (C)': 'temp'})
 
 # smoothen the position data and show the unsmoothed and smoothed data on the same graph
-axes = plt.gca()
+axes = utils.plt.gca()
 experiment_data.plot(x='time', y='pos', label='raw position data', ax=axes)
 # smoothen the position data via applying a rolling average
 experiment_data.pos = experiment_data.pos.rolling(window=20, center=True).mean()
 
 experiment_data.plot(x='time', y='pos', label='smoothened position', ax=axes, grid=True)
-plt.show()
+utils.plt.show()
