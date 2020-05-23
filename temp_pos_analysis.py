a70a42d41df6080cc2a4cc4c51d0528989b98c72
YonatanGideoni <yonatan.gideoni@gmail.com>
2020-05-23 10:39:58 +0300

    Made amplitude not be a calculated param

--- temp_pos_analysis.py
+++ temp_pos_analysis.py
@@ -18,23 +18,25 @@ def read_experiment_data(file_path=DEFAULT_FILE_PATH, header_row=DEFAULT_HEADER_
 
 
 def plot_temp_over_time_data(experiment_data, axes, fit_func_to_data=True, start_fit_time=0):
-    experiment_data.plot(x='time', y='temp', grid=True, linestyle='None', marker='.', label='Experimental data',
-                         ax=axes)
+    temp_amplitude = experiment_data.temp.max() - experiment_data.temp.min()
+
+    experiment_data.plot(x='time', y='temp', grid=True, linestyle='None',
+                         marker='.', label='Experimental data', ax=axes)
 
     start_fit_time = 0
     fit_data = experiment_data[experiment_data.time > start_fit_time]
     start_fit_temp = fit_data.temp.iat[0]
 
     if fit_func_to_data:
-        def temp_over_time_func(t, amplitude, decay_time):
-            return amplitude * (1 - np.exp(-(t - start_fit_time) / decay_time)) + start_fit_temp
+        def temp_over_time_func(t, decay_time):
+            return temp_amplitude * (1 - np.exp(-(t - start_fit_time) / decay_time)) + start_fit_temp
 
-        [amplitude, decay_time], errors = fit_func(temp_over_time_func, fit_data.time, fit_data.temp)
-        print(amplitude, decay_time, errors)
+        [decay_time], errors = fit_func(temp_over_time_func, fit_data.time, fit_data.temp)
+        print(temp_amplitude, decay_time, errors)
         fitted_data_label = 'Function fitted to data \n' \
-            f'T(t)=({amplitude:.2f}$\pm${errors[0]:.2f})(1-exp(-t/({decay_time:.0f}$\pm${errors[1]:.0f})))+' \
+            f'T(t)={temp_amplitude:.1f}(1-exp(-t/({decay_time:.0f}$\pm${errors[0]:.0f})))+' \
             f'{start_fit_temp}'
-        plot_func(lambda t: temp_over_time_func(t, amplitude, decay_time), axes=axes,
+        plot_func(lambda t: temp_over_time_func(t, decay_time), axes=axes,
                   x_range=[start_fit_time, fit_data.time.iat[-1]], c='k', label=fitted_data_label)
 
     axes.legend()
