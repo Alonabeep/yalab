041f4e9cd200d02e64d2cfb41eadef7336d0ae42
YonatanGideoni <yonatan.gideoni@gmail.com>
2020-05-23 10:53:52 +0300

    Added option to linearize data

--- temp_pos_analysis.py
+++ temp_pos_analysis.py
@@ -17,11 +17,8 @@ def read_experiment_data(file_path=DEFAULT_FILE_PATH, header_row=DEFAULT_HEADER_
     return experiment_data
 
 
-def plot_temp_over_time_data(experiment_data, axes, fit_func_to_data=True, start_fit_time=0):
+def plot_temp_over_time_data(experiment_data, axes, fit_func_to_data=True, start_fit_time=0, linearize=False):
     temp_amplitude = experiment_data.temp.max() - experiment_data.temp.min()
-
-    experiment_data.plot(x='time', y='temp', grid=True, linestyle='None',
-                         marker='.', label='Experimental data', ax=axes)
 
     start_fit_time = 0
     fit_data = experiment_data[experiment_data.time > start_fit_time]
@@ -36,10 +33,19 @@ def plot_temp_over_time_data(experiment_data, axes, fit_func_to_data=True, start
         fitted_data_label = 'Function fitted to data \n' \
             f'T(t)={temp_amplitude:.1f}(1-exp(-t/({decay_time:.0f}$\pm${errors[0]:.0f})))+' \
             f'{start_fit_temp}'
-        plot_func(lambda t: temp_over_time_func(t, decay_time), axes=axes,
-                  x_range=[start_fit_time, fit_data.time.iat[-1]], c='k', label=fitted_data_label)
+        if not linearize:
+            func_to_plot = lambda t: temp_over_time_func(t, decay_time)
+            plot_range = [start_fit_time, fit_data.time.iat[-1]]
+        else:
+            experiment_data['linearized_time'] = 1 - np.exp(-experiment_data.time / decay_time)
+            func_to_plot = lambda linear_t: temp_amplitude * linear_t + start_fit_temp
+            plot_range = [0, 1]
 
+        plot_func(func_to_plot, axes=axes, x_range=plot_range, c='k', label=fitted_data_label, zorder=12)
+
+    experiment_data.plot(x='time' if not linearize else 'linearized_time', y='temp', grid=True, linestyle='None',
+                         marker='.', label='Experimental data', ax=axes)
     axes.legend()
-    axes.set_xlabel('Time[s]')
+    axes.set_xlabel('Time[s]' if not linearize else f'1-exp(-t/{decay_time:.2f})')
     axes.set_ylabel('Temperature[$\degree$C]')
     axes.set_title('T(t) while heating plate is turned on')
