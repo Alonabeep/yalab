a08d9640fbc0b30e95723ad5b1f90af120428cfa
YonatanGideoni <yonatan.gideoni@gmail.com>
2020-05-26 21:37:54 +0300

    Fixed data

--- evaporation_rate_analysis.py
+++ evaporation_rate_analysis.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    experiment_data = {'rate_of_evaporation': [1.35e-6, 2.24e-6, 4.13e-6, 8.54701e-06],
+    experiment_data = {'rate_of_evaporation': [1.35e-6, 2.24e-6, 4.13e-6, 8.63e-6],
                        'water_temp': [70, 79, 91, 97],
-                       'roe_error': [1.16e-8, 2.03e-8, 4.56e-8, 0],
-                       'temp_error': [1.5] * 3 + [0.5]}
+                       'roe_error': [1.16e-8, 2.03e-8, 4.56e-8, 2.79e-7],
+                       'temp_error': [1.5] * 3 + [1]}
     home_experiment_data = {'rate_of_evaporation': [1.88e-5, 6.17e-6, 1.63e-5],
                             'water_temp': [100.3, 101.4, 102],
                             'roe_error': [1.36e-6, 6.2e-8, 2.28e-7],
